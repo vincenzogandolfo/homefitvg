ecebe2f78556f6dce5d026a46339f500290c0023
vg_dev <vincenzo.gandolfo7@gmail.com>
2023-11-10 20:05:01 +0100

    removed SafeArea

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -27,52 +27,50 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(60),
-              bottomRight: Radius.circular(60),
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(40),
+            bottomRight: Radius.circular(40),
           ),
-          backgroundColor: Colors.greenAccent,
-          elevation: 16,
-          title: const Text(
-            'HomeFit VG',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
+        ),
+        backgroundColor: Colors.greenAccent,
+        elevation: 16,
+        title: const Text(
+          'HomeFit VG',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
           ),
-          centerTitle: true,
         ),
-        body: _pages[_selectedIndex],
-        bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(FontAwesomeIcons.solidHeart),
-              label: 'Motivazione',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(FontAwesomeIcons.dumbbell),
-              label: 'Allenamento',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(FontAwesomeIcons.solidMessage),
-              label: 'Contatti',
-            ),
-          ],
-          iconSize: 16,
-          currentIndex: _selectedIndex,
-          selectedItemColor: Colors.black,
-          unselectedItemColor: Colors.white,
-          selectedIconTheme: const IconThemeData(size: 20),
-          selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
-          backgroundColor: Colors.greenAccent,
-          onTap: _onItemTapped,
-        ),
+        centerTitle: true,
+      ),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.solidHeart),
+            label: 'Motivazione',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.dumbbell),
+            label: 'Allenamento',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.solidMessage),
+            label: 'Contatti',
+          ),
+        ],
+        iconSize: 16,
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.white,
+        selectedIconTheme: const IconThemeData(size: 20),
+        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
+        backgroundColor: Colors.greenAccent,
+        onTap: _onItemTapped,
       ),
     );
   }
